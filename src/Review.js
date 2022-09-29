e113c6d4d5a02900d5031d53fe1d916ad21cbefc
otiisaac199 <otichinazaisaac19@gmail.com>
2022-09-29 21:36:30 +0100

    initial commit

--- src/Review.js
+++ src/Review.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
-  return <h2>review component</h2>;
+  return <h2></h2>;
 };
 
 export default Review;
