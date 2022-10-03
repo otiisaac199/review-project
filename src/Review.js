b4839a5be879fbf8aa3722cfe1a70626a8a60aae
otiisaac199 <otichinazaisaac19@gmail.com>
2022-10-03 10:40:44 +0100

    added the review info

--- src/Review.js
+++ src/Review.js
@@ -6,7 +6,28 @@ const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
 
-  return <h2>review component</h2>;
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className="prev-btn ">
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn ">
+          <FaChevronRight />
+        </button>
+        <button className="random-btn ">surprise me</button>
+      </div>
+    </article>
+  );
 };
 
 export default Review;
